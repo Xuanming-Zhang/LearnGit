d26e1731da84c63dee5c9a90804421eab6f951d0
Xuanming-Zhang <xuanming@usc.edu>
2022-07-18 12:19:22 +0800

    完善方法细则

--- HelloGit.py
+++ HelloGit.py
@@ -3,9 +3,9 @@ print("hello git!")
 # 练习回退操作
 
 # 已commit，没有push
-# git reset --soft 撤销commit
-# git reset --mixed 撤销commit和add
+# git reset --soft 目标版本号 撤销commit
+# git reset --mixed 目标版本号 撤销commit和add
 
 # 已push
-# git reset --hard 撤销并舍弃版本号之后的记录，谨慎使用
+# git reset --hard 目标版本号 撤销并舍弃版本号之后的记录，谨慎使用。 本地文件回滚之后需要使用 git push -f 强制回滚远程仓库
 # git revert 撤销但是保留了提交记录
