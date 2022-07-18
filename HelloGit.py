a743e6c12f43306e0bc38ddce64c9b88d0dd1466
Xuanming-Zhang <xuanming@usc.edu>
2022-07-18 11:39:34 +0800

    练习回滚操作——方法

--- HelloGit.py
+++ HelloGit.py
@@ -1,1 +1,11 @@
 print("hello git!")
+
+# 练习回退操作
+
+# 已commit，没有push
+# git reset --soft 撤销commit
+# git reset --mixed 撤销commit和add
+
+# 已push
+# git reset --hard 撤销并舍弃版本号之后的记录，谨慎使用
+# git revert 撤销但是保留了提交记录
